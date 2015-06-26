a9a4f5a30109ae7b22b3ba9f756ce0cbaff4fae1
Yury Dyumkin <y.dyumkin@itransition.com>
2015-06-26 16:52:59 +0300

    init module

--- config/web.php
+++ config/web.php
@@ -38,8 +38,28 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
+
     ],
     'params' => $params,
+
+    'modules' => [
+        'main' => [
+            'class' => 'app\modules\monopaint\Module',
+        ],
+    ],
 ];
 
 if (YII_ENV_DEV) {
---
+++ modules/monopaint/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\monopaint;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\monopaint\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
